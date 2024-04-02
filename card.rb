afcafbd7698626a0afc08a79bf8031f74a477ae9
teosevenses <141769461+teosevenses@users.noreply.github.com>
2024-04-02 23:27:57 +0500

    Update card.rb

--- card.rb
+++ card.rb
@@ -3,17 +3,23 @@ class Card
   SUITS = ['♠', '♣', '♥', '♦'].freeze
   NOMINALS = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
 
-  attr_reader :suit, :rank
+  attr_reader :suit, :nominal
 
   def initialize(suit, nominal)
     @suit = suit
     @nominal = nominal
   end
 
-  def value 
-    return 10 if %w[J Q K].include?(@nominal) 
-    return [1, 11] if @nominal == "A" 
-    @nominal.to_i 
+  def value(current_result)
+    return 10 if %w[J Q K].include?(@nominal)
+    return ace_value(current_result) if @nominal == "A"
+
+    @nominal.to_i
+  end
+
+  def ace_value(current_result)
+    return 1 if current_result + 11 > 21
+    11 
   end
 
   def to_s
